8b9c53206fbd97104737f7fe5ee46890d113c11b
Tactless7 <blier.juliane@gmail.com>
2019-04-14 20:31:54 +0200

    feat(port): Changed port to match Heroku's env variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json()); // for parsing application/json
 
