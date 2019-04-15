769e302de3a95b06d8032f3934e4f2bd1825cf2a
Tactless7 <blier.juliane@gmail.com>
2019-04-15 17:58:37 +0200

    feat(cors): Added cors to project

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json()); // for parsing application/json
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
 app.get('/restos', (req, res) => {
   const restos = fs.readFileSync(__dirname + '/restos.json', 'utf8');
   res.setHeader('Content-Type', 'application/json');
