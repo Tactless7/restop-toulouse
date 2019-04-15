c7205a9d4e00b6039fd349fccd01531b7e53533c
Tactless7 <blier.juliane@gmail.com>
2019-04-15 18:08:52 +0200

    reset json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT;
 
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json()); // used for parsing application/json
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
